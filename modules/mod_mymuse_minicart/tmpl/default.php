720620aa2bbd10409b88edfd38a4bffa1617dff5
gfisch <gfisch@a8f9a886-ea73-4d82-9a89-2293c992a55b>
2015-07-22 14:43:51 +0000

    update mini-cart for phones, sync text with regular cart

--- modules/mod_mymuse_minicart/tmpl/default.php
+++ modules/mod_mymuse_minicart/tmpl/default.php
@@ -6,10 +6,10 @@ $i = 0;
 ?>
 <table class="mymuse_cart">
 <thead>
-<tr class="sectiontableheader mymuse_cart_top">
-	<th class="sectiontableheader mymuse_cart_top"><?php echo JText::_('MYMUSE_ITEM') ?></th>
-	<th class="sectiontableheader mymuse_cart_top"><?php echo JText::_('MYMUSE_CART_QUANTITY') ?></th>
-	<th class="sectiontableheader mymuse_cart_top"><?php echo JText::_('MYMUSE_CART_SUBTOTAL') ?></th>
+<tr class="mymuse_cart">
+	<th class="mytitle"><?php echo JText::_('MYMUSE_TITLE') ?></th>
+	<th class="myquantity"><?php echo JText::_('MYMUSE_CART_QUANTITY') ?></th>
+	<th class="mysubtotal"><?php echo JText::_('MYMUSE_CART_SUBTOTAL') ?></th>
 </tr>
 </thead>
 <?php foreach($order->items as $item) { ?>
@@ -19,27 +19,29 @@ $i = 0;
 		<td class="myprice"><?php echo MyMuseHelper::printMoney($item->product_item_subtotal); ?></td>
 	</tr>
 <?php }?>
+
+	<tr>
+		 <td class="mobile-hide" colspan="2"><?php echo JText::_('MYMUSE_CART_SUBTOTAL'); ?>:</td>
+		 <td class="myoriginalsubtotal"><?php echo MyMuseHelper::printMoney($order->discount + $order->order_subtotal); ?></td>
+	</tr>
 		
 		<?php 
 		if($order->discount > 0.00){ 
 			//for shopper group discount
 			
 			?>
-			<tr>
-		    	<td class="mobile-hide" colspan="2"><?php echo JText::_('MYMUSE_CART_ORIGINAL_SUBTOTAL'); ?>:</td>
-		        <td class="myoriginalsubtotal"><?php echo MyMuseHelper::printMoney($order->discount + $order->order_subtotal); ?></td>
-		    </tr>
+			
 		    
 		    <tr>
-		    	<td colspan="2"><?php echo JText::_('MYMUSE_SHOPPING_GROUP_DISCOUNT'); ?>:
+		    	<td class="mobile-hide" colspan="2"><?php echo JText::_('MYMUSE_CART_ORIGINAL_SUBTOTAL'); ?>:
 		    	<?php echo $order->shopper_group_name; ?> <?php echo $order->shopper_group_discount; ?> %</td>
-		        <td class="mydiscount"><?php echo MyMuseHelper::printMoney($order->discount); ?></td>
+		        <td class="myoriginalsubtotal"><?php echo MyMuseHelper::printMoney($order->discount + $order->order_subtotal); ?></td>
 
 		    </tr>
 		    
 		    <tr>
-		    	<td class="mobile-hide" colspan="2"><?php echo JText::_('MYMUSE_CART_NEW_SUBTOTAL'); ?>:</td>
-		        <td class="mynewsubtotal"><?php echo MyMuseHelper::printMoney($order->order_subtotal); ?></td>
+		    	<td class="mobile-hide" colspan="2"><?php echo JText::_('MYMUSE_SHOPPING_GROUP_DISCOUNT'); ?>:</td>
+		        <td class="myshoppergroupdiscount"><?php echo MyMuseHelper::printMoney($order->discount); ?></td>
 
 		    </tr>
 		
@@ -50,8 +52,8 @@ $i = 0;
 		<?php //COUPONS
 		if($params->get("my_use_coupons") && @$order->coupon->id){ ?>
 		    <tr>
-		    	<td colspan="2"><?php echo JText::_('MYMUSE_YOUR_COUPON'); ?> : <?php echo $order->coupon->title ?></td>
-		        <td class="mydiscount">-<?php echo MyMuseHelper::printMoney($order->coupon->discount); ?> </td>
+		    	<td class="mobile-hide" colspan="2"><?php echo JText::_('MYMUSE_YOUR_COUPON'); ?> : <?php echo $order->coupon->title ?></td>
+		        <td class="mycoupon">-<?php echo MyMuseHelper::printMoney($order->coupon->discount); ?> </td>
 
 		    </tr>
 		<?php } ?>
@@ -59,8 +61,8 @@ $i = 0;
 		<?php // SHIPPING
 		if ($params->get("my_use_shipping") && @$order->order_shipping->cost > 0) { ?>
 		    <tr>
-		    	<td colspan="2" align="right"><b><?php echo JText::_('MYMUSE_SHIPPING') ?>:</b></td>
-		    	<td align="right"><?php echo MyMuseHelper::printMoney($order->order_shipping->cost); ?>
+		    	<td  class="mobile-hide" colspan="2" align="right"><b><?php echo JText::_('MYMUSE_SHIPPING') ?>:</b></td>
+		    	<td class="myshipping" align="right"><?php echo MyMuseHelper::printMoney($order->order_shipping->cost); ?>
 		    </td>
 
 		<?php } ?>
