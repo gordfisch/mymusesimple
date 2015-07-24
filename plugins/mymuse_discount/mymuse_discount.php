c759026a1879075f994cc77a2136d0536d2f0665
gfisch <gfisch@a8f9a886-ea73-4d82-9a89-2293c992a55b>
2015-07-24 15:42:20 +0000

    start on mymuse_discount plugin

---
+++ plugins/mymuse_discount/mymuse_discount.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * @version		$Id$
+ * @package		mymuse
+ * @copyright	Copyright © 2015 - Arboreta Internet Services - All rights reserved.
+ * @license		GNU/GPL
+ * @author		Gordon Fisch
+ * @author mail	info@mymuse.ca
+ * @website		http://www.mymuse.ca
+ */
+
+// no direct access
+defined( '_JEXEC' ) or die( 'Restricted access' );
+
+jimport( 'joomla.plugin.plugin');
+
+/**
+* MyMuse Shipping Price plugin
+*
+* @package 		MyMuse
+* @subpackage	mymuse
+*/
+class plgMymuseMymuse_discount extends JPlugin
+{
+	
+	/**
+	 * Load the language file on instantiation.
+	 *
+	 * @var    boolean
+	 * @since  3.1
+	 */
+	protected $autoloadLanguage = true;
+	
+	/**
+	 * Constructor
+	 *
+	 * @param   object  $subject  The object to observe
+	 * @param   array   $config   An array that holds the plugin configuration
+	 */
+	function plgMymuseMymuse_discount(&$subject, $config)  {
+		
+		parent::__construct($subject, $config);
+
+	}
+		
+		
+	/**
+	 * onBeforeMyMuseCheckout
+	 * if discount, modify cart
+	 * 
+	 *
+	 * @param object		$shopper
+	 * @param object		$store
+	 * @param object		$order
+	 * @param object		$params
+	 * 
+	 * returns true
+	 */
+	function onBeforeMyMuseCheckout($shopper, $store, $order, $params)
+	{
+
+        //var_dump($this->params); exit;
+		if($this->params->get('discount_based_on')){
+			//0 = units, 1 = subTotal
+			$target = $order->order_subtotal;
+		}else{
+			$target = $cart['idx'];
+		}
+		$discount_type = $this->params->get('discount_type'); //0 = amount, 1 = percent
+        $result = 0;
+        $j = 0;
+		for($i=1;$i<13;$i++){
+            $param = "discount_minimum".$i;
+            //is it active?
+            if($this->params->get($param, 0)){
+				$min = $this->params->get($param);
+				if($target > $min){
+					$discount = $this->params->get('discount_'.$i);
+					if($this->params->get('discount_type')){
+						//percent
+						$order->discount = $order->order_subtotal * $discount / 100;
+					}else{
+						//amount
+						$order->discount = $discount;
+					}
+				}
+            } 
+        }
+		return true;
+	}
+}
