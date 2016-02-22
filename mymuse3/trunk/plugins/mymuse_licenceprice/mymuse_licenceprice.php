31248ea7fa7bd7ace78ac70c7051b11a465a62eb
gfisch <gfisch@a8f9a886-ea73-4d82-9a89-2293c992a55b>
2016-02-22 01:16:06 +0000

    work on licenceprice plugin

--- mymuse3/trunk/plugins/mymuse_licenceprice/mymuse_licenceprice.php
+++ mymuse3/trunk/plugins/mymuse_licenceprice/mymuse_licenceprice.php
@@ -44,7 +44,8 @@ class plgMymuseMymuse_licenceprice extends JPlugin
 		
 		
 	/**
-	 * onBeforeMyMuseCheckout
+	 * 
+	 * onCalculatePrice
 	 * if licenceprice, modify cart
 	 * 
 	 *
@@ -55,44 +56,15 @@ class plgMymuseMymuse_licenceprice extends JPlugin
 	 * 
 	 * returns true
 	 */
-	function onCalculatePrice($order, $cart)
+	function onCalculatePrice($price_info, $cart)
 	{
 		$this_licenceprice = 0;
-		$quantity = 0;
-		if($this->params->get('licenceprice_based_on')){
-			//1 = subTotal
-			$target = $order->order_subtotal;
-		}else{
-			//0 = units,
-			$quantity = 0;
-			for ($i=0;$i<$cart["idx"];$i++) {
-				if(isset($cart[$i]['quantity'])){
-					$quantity += $cart[$i]['quantity'];
-				}
-			}
-			$target = $quantity;
-		}
-		$licenceprice_type = $this->params->get('licenceprice_type'); //0 = amount, 1 = percent
-        $result = 0;
-        $j = 0;
-		for($i=1;$i<13;$i++){
-            $param = "licenceprice_minimum_".$i;
-            if($this->params->get($param, 0)){
-				$min = $this->params->get($param);
-				if($target >= $min){
-					$licenceprice = $this->params->get('licenceprice_'.$i);
-					if($this->params->get('licenceprice_type')){
-						//percent
-						$this_licenceprice = $order->order_subtotal * $licenceprice / 100;
-					}else{
-						//amount
-						$this_licenceprice = $licenceprice;
-					}
+		$quantity = $cart["idx"];
+		$session = JFactory::getSession();
+		$my_licence = $jinput->get('my_licence',$session->get("my_licence",0));
+		
 
-				}
-            } 
-        }
 
-		return $this_licenceprice;
+		return true;
 	}
 }
