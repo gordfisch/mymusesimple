4792fb71a18514b0f72886cb12eb7701fc3170af
gfisch <gfisch@a8f9a886-ea73-4d82-9a89-2293c992a55b>
2016-06-08 20:40:14 +0000

    add mod_jplayer

---
+++ mymuse3/trunk/modules/mod_mymuse_jplayer/mod_mymuse_player.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @version		$Id$
+ * @package		mymuse
+ * @copyright	Copyright © 2011 - Arboreta Internet Services - All rights reserved.
+ * @license		GNU/GPL
+ * @author		Gordon Fisch
+ * @author mail	info@mymuse.ca
+ * @website		http://www.mymuse.ca
+ */
+
+// no direct access
+defined('_JEXEC') or die('Restricted access');
+
+$document = JFactory::getDocument();
+$this->direction = $document->direction;
+$site_url = preg_replace("#administrator/#","",JURI::base());
+
+if($this->direction == "rtl"){
+	$this->css_path = $site_url.'plugins/mymuse/audio_html5/skin/jplayer.blue.monday.rtl.css';
+}else{
+	$this->css_path = $site_url.'plugins/mymuse/audio_html5/skin/jplayer.blue.monday.css';
+}
+
+
+require(JModuleHelper::getLayoutPath('mod_mymuse_jplayer'));
