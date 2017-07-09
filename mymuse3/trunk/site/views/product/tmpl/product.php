2ac2b7f5f732aefc53dbeb7408c80538908d61a7
gfisch <gfisch@a8f9a886-ea73-4d82-9a89-2293c992a55b>
2017-07-09 13:49:34 +0000

    update orderby_track update cart to show continue shopping

--- mymuse3/trunk/site/views/product/tmpl/product.php
+++ mymuse3/trunk/site/views/product/tmpl/product.php
@@ -184,13 +184,11 @@ foreach($tracks as $track){
                     }else{
                         txt = idx+" "+"items";
                     }
-                    link = \''.'<a href="'.JRoute::_('index.php?option=com_mymuse&task=showcart&view=cart&Itemid='.$Itemid).'">'.JText::_('MYMUSE_VIEW_CART').'</a>\';
                     $("#carttop1").html(txt);
                     $("#carttop2").html(link);
                 }else{
 
                     $("#carttop1").html(" ");
-                    $("#carttop2").html("'.JText::_('MYMUSE_YOUR_CART_IS_EMPTY').'");
                 }
                 my_modal.open({content: msg+"<br />"+link, width: 300 });
             });
@@ -236,13 +234,10 @@ jQuery(document).ready(function($){
                     }else{
                         txt = idx+" "+"items";
                     }
-                    link = \''.'<a href="'.JRoute::_('index.php?option=com_mymuse&task=showcart&view=cart&Itemid='.$Itemid).'">'.JText::_('MYMUSE_VIEW_CART').'</a>\';
-                    $("#mini-cart-text").html(txt);
-                    $("#mini-cart-link").html(link);
+                    $("#carttop1").html(txt);
                 }else{
 	
-                    $("#mini-cart-text").html(" ");
-                    $("#mini-cart-link").html("'.JText::_('MYMUSE_YOUR_CART_IS_EMPTY').'");
+                    $("#carttop1").html(" ");
                 }
                 my_modal.open({content: msg+"<br />"+link, width: 300 });
             });
@@ -290,7 +285,7 @@ echo myMuseHelperRoute::getCategoryRoute($product->artist->parent_id); ?>">
 	<div class="mini-cart-top">
 		<div class="mini-cart-content">
 			<div class="mini-cart-cart"></div>
-			<div class="mini-cart-text"><?php
+			<div class="mini-cart-text" id="carttop1"><?php
 if($this->cart['idx']) :
     $word = ($this->cart['idx'] == 1) ? "item" : "items"; 
     echo $this->cart['idx']." $word";
